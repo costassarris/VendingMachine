32a744d6a7b6b288988713c389062560ca68fe4a
costassarris <costassarris@gmail.com>
2015-06-15 22:35:29 +0100

    accepts different coins and rejects invalid coins

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,11 +1,26 @@
 class VendingMachine
 
+  VALID_COINS = ['5', '10', '25']
+
+  attr_reader :display, :coin_return
+
+  def initialize
+    self.display = 'INSERT COIN'
+    self.coins = []
+  end
+
   def insert(coin)
-
+    if VALID_COINS.include? coin
+      @coins.push(coin)
+      self.display = "#{@coins.inject{|sum, x| sum.to_i + x.to_i}} cents"
+    else
+      self.coin_return = coin
+    end
   end
 
-  def display
-    '5 cents'
-  end
+  private
+
+  attr_accessor :coins
+  attr_writer :display, :coin_return
 
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,8 +1,44 @@
 require 'vending_machine'
 
 describe VendingMachine do
-  it 'accepts coins and updates display' do
-    subject.insert '5'
-    expect(subject.display).to eq '5 cents'
+  context 'valid coins' do
+    it 'accepts a nickel and updates display' do
+      subject.insert '5'
+      expect(subject.display).to eq '5 cents'
+    end
+
+    it 'accepts a dime and updates display' do
+      subject.insert '10'
+      expect(subject.display).to eq '10 cents'
+    end
+
+    it 'accepts a quarter and updates display' do
+      subject.insert '25'
+      expect(subject.display).to eq '25 cents'
+    end
+
+    it 'accepts coin combo and updates display' do
+      subject.insert '5'
+      subject.insert '10'
+      subject.insert '25'
+      expect(subject.display).to eq '40 cents'
+    end
+  end
+
+  context 'invalid coins' do
+
+    it 'rejects and returns a penny' do
+      subject.insert '1'
+      expect(subject.coin_return).to eq '1'
+    end
+
+    it 'rejects and returns anything that is not a coin' do
+      subject.insert '1asdas'
+      expect(subject.coin_return).to eq '1asdas'
+    end
+
+    it 'displays INSERT COIN when no coins inserted' do
+      expect(subject.display).to eq 'INSERT COIN'
+    end
   end
 end
